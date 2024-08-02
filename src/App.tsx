b3d13dd9970faea47d64893aeb28c5faae3fb768
Mileto <milenkotomasevic41@gmail.com>
2024-08-01 23:53:36 -0400

    dev: Added main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-function App() {
+import PatientForm from './components/PatientForm';
+import PatientList from './components/PatientList';
 
-  return (
-    <>
-      <h1 className="text-3xl">Pacientes</h1>
-    </>
-  )
+function App() {
+	return (
+		<>
+			<div className='container mx-auto mt-20 gato'>
+				<h1 className='font-black text-5xl text-center md:w-2/3 md:mx-auto'>
+					Seguimiento de pacientes {''}
+					<span className='text-indigo-700'>Veterinaria</span>
+				</h1>
+				<div className='mt-12 md:flex'>
+					<PatientForm />
+					<PatientList />
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
