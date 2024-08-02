80bc38dbe50ab7d63ee1e086fa9542bdd3ec9636
Mileto <milenkotomasevic41@gmail.com>
2024-08-02 00:50:35 -0400

    dev: Added info in list to view patients

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,5 +1,7 @@
-
+import { usePatientStore } from "../store/store"
 export default function PatientList() {
+    const patients = usePatientStore((state) => state.patients);
+    console.log(patients)
   return (
     <div>PatientList</div>
   )
