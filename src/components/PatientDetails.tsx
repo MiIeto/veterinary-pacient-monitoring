08c174cf7d993aa78b3c215a8f317800d559248d
Mileto <milenkotomasevic41@gmail.com>
2024-08-02 01:26:52 -0400

    dev: Added getpatientid to Edit button

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -6,7 +6,7 @@ type PatientDetailsProps = {
 	patient: Patient;
 };
 export default function PatientDetails({ patient }: PatientDetailsProps) {
-	const deletePatient = usePatientStore((state) => state.deletePatient);
+	const { deletePatient, getPatientById } = usePatientStore();
 	return (
 		<div className='mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl'>
 			<PatientDetailItem
@@ -38,6 +38,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
 				<button
 					type='button'
 					className='py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg'
+					onClick={() => getPatientById(patient.id)}
 				>
 					Editar
 				</button>
